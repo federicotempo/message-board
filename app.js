7f0902cd962e5e0074a5af8a4e6735b48bdca564
federicotempo <federicotempo98@gmail.com>
2025-01-22 16:24:22 -0300

    Add assetsPath

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const PORT = 8080;
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
 app.use(express.urlencoded({ extended: true }));
 
 const indexRouter = require("./routes/indexRouter");
