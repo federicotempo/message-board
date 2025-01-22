65a19dca7d6f7de98801d01193477d3a1a4447e3
federicotempo <federicotempo98@gmail.com>
2025-01-22 14:51:22 -0300

    Update indexRouter
    -Configures indexRouter.js

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,8 +1,29 @@
 const { Router } = require("express");
+const { addNewMessage } = require("../controllers/formController");
+
 indexRouter = Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
 indexRouter.get("/", (req, res) => {
-  res.send("Welcome to message app");
+  res.render("index", { messages });
 });
+
+indexRouter.get("/new", (req, res) => {
+  res.render("form");
+});
+
+indexRouter.post("/new", addNewMessage(messages));
 
 module.exports = indexRouter;
