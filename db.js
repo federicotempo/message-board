21a31e1df15acf626ac8db2eb0e317285b6b2535
federicotempo <federicotempo98@gmail.com>
2025-01-23 08:44:28 -0300

    Add new messages for testing
    -Adds new objects messages

--- db.js
+++ db.js
@@ -1,12 +1,47 @@
 const messages = [
   {
-    text: "Hi there!",
+    text: "Hi there! How are you doing today?",
     user: "Amando",
     added: new Date(),
   },
   {
-    text: "Hello World!",
+    text: "Hello World! This is a great day to learn something new.",
     user: "Charles",
+    added: new Date(),
+  },
+  {
+    text: "Good morning everyone! Hope you all have a fantastic day ahead.",
+    user: "Jessica",
+    added: new Date(),
+  },
+  {
+    text: "Just finished reading a great book. Highly recommend it to everyone.",
+    user: "Michael",
+    added: new Date(),
+  },
+  {
+    text: "Started a new project at work. Excited to see how it turns out.",
+    user: "David",
+    added: new Date(),
+  },
+  {
+    text: "Just baked some delicious cookies. Can't wait to share them with friends.",
+    user: "Sarah",
+    added: new Date(),
+  },
+  {
+    text: "Went for a long walk this evening. It was very refreshing.",
+    user: "John",
+    added: new Date(),
+  },
+  {
+    text: "Learning to play the guitar. It's challenging but very rewarding.",
+    user: "Anna",
+    added: new Date(),
+  },
+  {
+    text: "Had a great workout session today. Feeling energized and strong.",
+    user: "James",
     added: new Date(),
   },
 ];
